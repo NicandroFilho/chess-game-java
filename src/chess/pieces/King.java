2e7a6b036ca4d33262d7912b82c1ab6f2d977c4a
NicandroFilho <nicandro.m@gmail.com>
2019-08-29 18:46:00 -0300

    Knight

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -72,10 +72,6 @@ public class King extends ChessPiece {
             mat[pAux.getRow()][pAux.getColumn()] = true;
         }
 
-
-
-
-
         return mat;
     }
 
---
+++ src/chess/pieces/Knight.java
@@ -0,0 +1,81 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Knight extends ChessPiece {
+    public Knight(Board board, Color color) {
+        super(board, color);
+    }
+
+
+    /*Can move if the target position is empty or the piece at target position is an Opponents Piece*/
+    boolean canMove(Position targetPosition){
+        ChessPiece chessPiece = (ChessPiece)getBoard().piece(targetPosition);
+        return chessPiece==null || chessPiece.getColor() != getColor();
+    }
+
+    @Override
+    public String toString(){
+        return "N";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0, 0);
+
+        // Movement 1 - 2 North 1 East
+        p.setValues(position.getRow() - 2, position.getColumn() -1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Movement 2 - 2 North 1 West
+        p.setValues(position.getRow() - 2, position.getColumn() +1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Movement 3 - 2 South 1 East
+        p.setValues(position.getRow() + 2, position.getColumn() -1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Movement 4 - 2 South 1 West
+        p.setValues(position.getRow() + 2, position.getColumn() +1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Movement 5 - 2 East 1 North
+        p.setValues(position.getRow() - 1, position.getColumn() - 2);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Movement 6 - 2 East 1 South
+        p.setValues(position.getRow() + 1, position.getColumn() - 2);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Movement 7 - 2 West 1 North
+        p.setValues(position.getRow() - 1, position.getColumn() + 2);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Movement 8 - 2 West 1 South
+        p.setValues(position.getRow() + 1, position.getColumn() + 2);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
+    }
+}
